8f19eb79ad155e65920b5d57fcc39ac2f3603259
Kristjan Fale <kristjan.fale@gmail.com>
2020-04-06 01:22:20 +0200

    Output Users - forEach()

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ const main = document.getElementById('main');
 
 let data = [];
 
+// Get 3 users for start
 getUser();
 getUser();
 getUser();
 
+// Fetch user
 async function getUser() {
   let response = await fetch('https://randomuser.me/api/');
   let data = await response.json();
@@ -28,7 +30,34 @@ async function getUser() {
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
 
-// Event listeners
-addUser.addEventListener('click', () => {});
+// Update DOM (If nothing is passed in, use data array as default)
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+
+  // Loop through data and display users on DOM
+  providedData.forEach((user) => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${user.name}</strong> ${formatMoney(
+      user.money
+    )}$`;
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+function formatMoney(number) {
+  return number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+// Event Listeners
+addUser.addEventListener('click', getUser);
+double.addEventListener('click');
+showMillionaires.addEventListener('click');
+sort.addEventListener('click');
+calculateWealth.addEventListener('click');
